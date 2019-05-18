5a257afc300eddf298fdd23f33ac7feeb5ccf17d
krakopo <krakopo@protonmail.com>
2019-05-18 10:30:00 -0400

    Implement screen driver

---
+++ drivers/screen.h
@@ -0,0 +1,19 @@
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define WHITE_ON_BLACK 0x0f
+#define YELLOW_ON_BLACK 0x0e
+
+// Screen I/O ports
+#define REG_SCREEN_CTRL 0x3D4
+#define REG_SCREEN_DATA 0x3D5
+
+void print_char(char character, int col, int row, char attribute_byte);
+int get_cursor();
+void set_cursor(int offset);
+int get_screen_offset(int col, int row);
+int handle_scrolling(int offset);
+void print_at(char* message, int col, int row, char attribute_byte);
+void print(char *message);
+void print_with_attr(char *message, char attribute_byte);
+void clear_screen();
