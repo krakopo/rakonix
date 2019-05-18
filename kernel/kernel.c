b9185b74da184d7f1fd5c3000f28e68fbbb8dda9
krakopo <krakopo@protonmail.com>
2019-05-18 10:31:35 -0400

    Use new screen API to print a welcome message after the kernel is loaded

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,5 @@
+#include "drivers/screen.h"
+
 void main() {
-	// Video memory starts at 0xb8000
-	char* video_memory = (char*) 0xb8000;
-	// Display an 'X' in the top-left of the screen
-	*video_memory = 'X';
+	print_with_attr("\nWelcome to RakoNIX!\n", YELLOW_ON_BLACK);
 }
