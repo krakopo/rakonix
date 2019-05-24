ae94ed4d5ef0e6458ff93b254f85003436ede483
krakopo <krakopo@protonmail.com>
2019-05-24 18:35:46 -0400

    Enable the handling of interrupts

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,13 @@
 #include "drivers/screen.h"
 
-void main() {
+/* Forward declaration for function to setup IDT */
+void idt_install();
+
+void main()
+{
+  /* Print welcome banner */
   print_with_attr("\nWelcome to RakoNIX!\n", YELLOW_ON_BLACK);
+
+  /* Start handling interrupts */
+  idt_install();
 }
